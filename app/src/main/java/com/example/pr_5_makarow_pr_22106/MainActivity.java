db47297a2df7abfce93e71fe7681ff64aa0d1f2e
egor makarow <porem.578@gmail.com>
2023-03-10 11:33:44 +0700

    new int a

--- app/src/main/java/com/example/pr_5_makarow_pr_22106/MainActivity.java
+++ app/src/main/java/com/example/pr_5_makarow_pr_22106/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+int a = 1;
         setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
